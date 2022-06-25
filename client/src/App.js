13313754434ad0602cf6021476b85f0fb158c760
Kanishka Panwar <kanishkap2002@gmail.com>
2022-06-25 16:26:12 +0530

    activity card added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Card from "./components/card/Card";
 
 function App() {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <div classname="activitycard">
+        <div className="cardelement">
+          <Card
+            title="Rafting"
+            imageUrl="https://harstuff-travel.org/wp-content/uploads/2019/08/rafting.jpg"
+          />
+        </div>
+        <div className="cardelement">
+          <Card
+            title="Banana Boat"
+            imageUrl="https://cdn.shopify.com/s/files/1/0477/4055/1326/products/bananaboat.jpg?v=1602343901"
+          />
+        </div>
+        <div className="cardelement">
+          <Card
+            title="Hang Gliding"
+            imageUrl="https://img.traveltriangle.com/blog/wp-content/uploads/2020/02/og-for-Hang-gliding-in-Brazil_4th-feb.jpg"
+          />
+        </div>
+      </div>
+
+      <div className="postcard">login post explore</div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
